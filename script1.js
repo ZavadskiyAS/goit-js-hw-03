a9526746e4268f40f2a885b12d80ea1fcc3fe0be
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-22 22:11:10 +0300

    task1

--- script1.js
+++ script1.js
@@ -1,7 +1,21 @@
-"use strict";
+// Напиши скрипт, который, для объекта user, последовательно:
+
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+debugger;
 const user = {
   name: 'Mango',
   age: 20,
   hobby: 'html',
   premium: true,
 };
+user.hobby = 'skydiving';
+user.premium = false;
+user.mood = 'happy';
+
+const keys = Object.keys(user);
+for (const key of keys) {
+  console.log(key , user[key]);
+}
