865c9e817bd8a28113e6b333dc8409e292e9bef7
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-24 15:03:34 +0300

    task 3-6

--- script1.js
+++ script1.js
@@ -1,21 +1,20 @@
-// Напиши скрипт, который, для объекта user, последовательно:
+// // Напиши скрипт, который, для объекта user, последовательно:
 
-// добавляет поле mood со значением 'happy'
-// заменяет значение hobby на 'skydiving'
-// заменяет значение premium на false
-// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
-debugger;
-const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
-};
-user.hobby = 'skydiving';
-user.premium = false;
-user.mood = 'happy';
+// // добавляет поле mood со значением 'happy'
+// // заменяет значение hobby на 'skydiving'
+// // заменяет значение premium на false
+// // выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+// const user = {
+//   name: 'Mango',
+//   age: 20,
+//   hobby: 'html',
+//   premium: true,
+// };
+// user.hobby = 'skydiving';
+// user.premium = false;
+// user.mood = 'happy';
 
-const keys = Object.keys(user);
-for (const key of keys) {
-  console.log(key , user[key]);
-}
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(key , user[key]);
+// }
