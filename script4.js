50d70a598e0b09ec89ae485b16bde410e5850639
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-27 11:45:45 +0300

    -//

--- script4.js
+++ script4.js
@@ -5,35 +5,35 @@
 // ее. Каждое поле объекта, передаваемого в
 // функцию, имеет вид "имя":"зарплата".
 
-// const countTotalSalary = function(employees) {
-//   const values = Object.values(employees);
-//   //console.log(values);
+const countTotalSalary = function(employees) {
+  const values = Object.values(employees);
+  console.log(values);
 
-//   let total = 0;
+  let total = 0;
 
-//   for (const value of values) {
-//     total += value;
-//   }
-//   return total;
-// };
+  for (const value of values) {
+    total += value;
+  }
+  return total;
+};
 
-// console.log(countTotalSalary({})); // 0
+console.log(countTotalSalary({})); // 0
 
-// console.log(
-//   countTotalSalary({
-//     mango: 100,
-//     poly: 150,
-//     alfred: 80
-//   })
-// ); // 330
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80
+  })
+); // 330
 
-// console.log(
-//   countTotalSalary({
-//     kiwi: 200,
-//     lux: 50,
-//     chelsy: 150
-//   })
-// ); // 400
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150
+  })
+); // 400
 
 //===================================================================================
 
@@ -46,9 +46,9 @@
 //     return total;
 //   };
   
-//   /*
-//    * Вызовы функции для проверки работоспособности твоей реализации.
-//    */
+// //   /*
+// //    * Вызовы функции для проверки работоспособности твоей реализации.
+// //    */
 //   console.log(countTotalSalary({})); // 0
   
 //   console.log(
--- script5.js
+++ script5.js
@@ -1,31 +1,28 @@
-// "use strict";
+"use strict";
 // // Напиши функцию getAllPropValues(arr, prop),
 // // которая получает массив объектов и имя свойства.
 // // Возвращает массив значений определенного
 // // свойства prop из каждого объекта в массиве.
 
-// const products = [   
-//     { name: "Радар", price: 1300, quantity: 4 },
-//     { name: "Сканер", price: 2700, quantity: 3 },
-//     { name: "Дроид", price: 400, quantity: 7 },
-//     { name: "Захват", price: 1200, quantity: 2 }
-//   ];
+const products = [   
+    { name: "Радар", price: 1300, quantity: 4 },
+    { name: "Сканер", price: 2700, quantity: 3 },
+    { name: "Дроид", price: 400, quantity: 7 },
+    { name: "Захват", price: 1200, quantity: 2 }
+  ];
   
-//   const getAllPropValues = function(arr, prop) {
-//     const newArr = [];
-//     for (const product of products) {
-//       if (product[prop]) {
-//         newArr.push(product[prop]);
-//       }
-//     }
-//     return newArr;
-//   };
+  const getAllPropValues = function(arr, prop) {
+    const newArr = [];
+    for (const product of products) {
+      if (product[prop]) {
+        newArr.push(product[prop]);
+      }
+    }
+    return newArr;
+  };
   
-//   /*
-//    * Вызовы функции для проверки работоспособности твоей реализации.
-//    */
-//   console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+  console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
   
-//   console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+  console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
   
-//   console.log(getAllPropValues(products, "category")); // []
+  console.log(getAllPropValues(products, "category")); // []
