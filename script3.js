810eaf240e0d5625909608d9809ce182c51a2413
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-04-27 11:49:35 +0300

    2 and 3 two

--- script3.js
+++ script3.js
@@ -47,44 +47,44 @@ console.log(
 
 
 
-// const findBestEmployee = function(employees) {
-//     let name;
-//     let max = 0;
-//     const bestEmployeeArr = Object.entries(employees);
-//     for (const [employee, task] of bestEmployeeArr) {
-//       if (max < task) {
-//         max = task;
-//         name = employee;
-//       }
-//     }
-//     return name;
-//   };
+const findBestEmployee = function(employees) {
+    let name;
+    let max = 0;
+    const bestEmployeeArr = Object.entries(employees);
+    for (const [employee, task] of bestEmployeeArr) {
+      if (max < task) {
+        max = task;
+        name = employee;
+      }
+    }
+    return name;
+  };
   
 //   /*
 //    * Вызовы функции для проверки работоспособности твоей реализации.
 //    */
-//   console.log(
-//     findBestEmployee({
-//       ann: 29,
-//       david: 35,
-//       helen: 1,
-//       lorence: 99,
-//     }),
-//   ); // lorence
+  console.log(
+    findBestEmployee({
+      ann: 29,
+      david: 35,
+      helen: 1,
+      lorence: 99,
+    }),
+  ); // lorence
   
-//   console.log(
-//     findBestEmployee({
-//       poly: 12,
-//       mango: 17,
-//       ajax: 4,
-//     }),
-//   ); // mango
+  console.log(
+    findBestEmployee({
+      poly: 12,
+      mango: 17,
+      ajax: 4,
+    }),
+  ); // mango
   
-//   console.log(
-//     findBestEmployee({
-//       lux: 147,
-//       david: 21,
-//       kiwi: 19,
-//       chelsy: 38,
-//     }),
-//   ); // lux
+  console.log(
+    findBestEmployee({
+      lux: 147,
+      david: 21,
+      kiwi: 19,
+      chelsy: 38,
+    }),
+  ); // lux
--- script4.js
+++ script4.js
@@ -37,32 +37,32 @@ console.log(
 
 //===================================================================================
 
-// const countTotalSalary = function(employees) {
-//     let total = 0;
+const countTotalSalary = function(employees) {
+    let total = 0;
   
-//     for (const employee in employees) {
-//       total += employees[employee];
-//     }
-//     return total;
-//   };
+    for (const employee in employees) {
+      total += employees[employee];
+    }
+    return total;
+  };
   
 // //   /*
-// //    * Вызовы функции для проверки работоспособности твоей реализации.
-// //    */
-//   console.log(countTotalSalary({})); // 0
+//    * Вызовы функции для проверки работоспособности твоей реализации.
+//    */
+  console.log(countTotalSalary({})); // 0
   
-//   console.log(
-//     countTotalSalary({
-//       mango: 100,
-//       poly: 150,
-//       alfred: 80,
-//     }),
-//   ); // 330
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
   
-//   console.log(
-//     countTotalSalary({
-//       kiwi: 200,
-//       lux: 50,
-//       chelsy: 150,
-//     }),
-//   ); // 400
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
